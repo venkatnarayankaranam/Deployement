6868001016b200006e78d5a4a2e2fa0fbb039cb0
Pallavibandreddy <pallavibandreddy6@gmail.com>
2025-08-04 11:45:08 +0530

    workflow

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from prometheus_client import Counter, generate_latest, Gauge
 
 app = Flask(__name__)
 
@@ -8,9 +9,14 @@ income_entries = [
     {"source": "Freelance", "amount": 15000}
 ]
 
+# PROMETHEUS METRICS
+income_add_counter = Counter('income_add_requests', 'Number of income add requests')
+total_income_gauge = Gauge('total_income_amount', 'Total income amount')
+
 @app.route('/')
 def index():
     total = sum(entry['amount'] for entry in income_entries)
+    total_income_gauge.set(total)
     return render_template("index.html", income=income_entries, total=total)
 
 @app.route('/add', methods=['POST'])
@@ -18,7 +24,12 @@ def add_income():
     source = request.form['source']
     amount = float(request.form['amount'])
     income_entries.append({"source": source, "amount": amount})
+    income_add_counter.inc()
     return index()
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
